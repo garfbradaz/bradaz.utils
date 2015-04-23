67f326f30b8e3307fc872707410b1c2069b5351c
garfbradaz <zimzum17_1998@hotmail.com>
2015-04-23 20:25:40 +0100

    1st commit

---
+++ Bradaz.Utils/Class1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Bradaz.Utils.Interfaces;
+
+namespace Bradaz.Utils
+{
+    public class CSVReader : ICSV,    
+    {
+
+    }
+}
