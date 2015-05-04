620fb1cbe476739867924735a694e76474a5ac38
garfbradaz <zimzum17_1998@hotmail.com>
2015-05-04 22:07:53 +0100

    New Class Files and example applications

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Bradaz.Utils.IO;
+using Bradaz.Utils;
+
+namespace Test_CSV
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                string myFile = @"C:\Users\Gareth\SkyDrive\CODE\Bradaz.Utils\ConsoleApplication1\testfile.txt";
+                CSVFile file = new CSVFile(myFile);
+         
+                using (file.CSVStream = new CSVReader(file.FileNameAndPath))
+                {
+                    file.ReadLines();
+
+                    //sort so we get the Validated first.
+   
+                    foreach(CSVRow row in file.Rows)
+                    {
+                        Console.WriteLine("==========");
+                        Console.WriteLine("Row No: " + row.RowNumber + " has " + row.NumberOfColumns + " columns. The data is " + row.OriginalData);
+                        Console.WriteLine("Validation: ");
+                        Console.WriteLine("Row No: " + row.RowNumber + " " + row.State);
+
+                        foreach (RowError error in row.Errors)
+                        {
+                            Console.WriteLine("Error: " + error);
+
+                        }
+
+                        if (row.NumberOfColumns > 0)
+                        {
+                            foreach(CSVColumn column in row.Columns)
+                            {
+                                Console.WriteLine("Column No: " + column.ColumnNumber + " for row " + column.RowNumber + " " + column.ColumnValue);
+                            }
+                        }
+ 
+                    }
+                    
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error reading file " + e.Message);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Test_CSV_WinForms/Form1.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Bradaz.Utils.IO;
+using Bradaz.Utils;
+
+namespace Test_CSV_WinForms
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                string myFile = @"C:\Users\Gareth\SkyDrive\CODE\Bradaz.Utils\ConsoleApplication1\testfile.txt";
+                CSVFile file = new CSVFile(myFile);
+                int index = 0;
+                
+                using (file.CSVStream = new CSVReader(file.FileNameAndPath))
+                {
+                    file.ReadLines();
+                    dataGridView1.ColumnCount = file.NumberOfColumns;
+
+
+                    foreach (CSVRow row in file.Rows)
+                    {
+                        if (row.State == State.Validated)
+                        {
+                            DataGridViewRow dataGridRow = new DataGridViewRow();
+                            dataGridRow.CreateCells(dataGridView1);
+
+                            foreach (CSVColumn column in row.Columns)
+                            {
+                                ///Set the columns out.
+                                if (column.RowNumber == 1)
+                                {
+                                    dataGridView1.Columns[column.ColumnNumber - 1].Name = column.ColumnValue;
+                                }
+                                else
+                                {
+                                    dataGridRow.Cells[column.ColumnNumber - 1].Value = column.ColumnValue;
+                                }
+                                
+                      
+                            }
+
+                            if(row.RowNumber > 1)
+                            {
+                                dataGridView1.Rows.Add(dataGridRow);
+                            }
+                          
+                        }
+                    }
+                }     
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error reading file " + ex.Message + " - ");
+            }
+        }
+
+        }
+}
