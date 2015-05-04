203b226d4b9554b6afbfb844eb1eb0a3801b5931
garfbradaz <zimzum17_1998@hotmail.com>
2015-05-04 22:07:53 +0100

    New Class Files and example applications

---
+++ Bradaz.Utils/Flags.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bradaz.Utils
+{
+
+    /// <summary>
+    /// State of a record.
+    /// </summary>
+        [Flags]
+        public enum State
+        {
+            Validated = 0,
+            Failed = 1,
+            LogicallyDeleted = 2
+
+        }
+
+        /// <summary>
+        /// These Row Errors will be found during Parsing the row.
+        /// They are based off the rules of the RFC4180.
+        /// </summary>
+        /// 
+        [Flags]
+        public enum RowError
+        {
+            RFC4180Passed = 0,
+            MoreColumnsThanPreviousRows = 1,
+            LastFieldFollowedByComma = 2,
+            ImbeddedDelimiterIssue = 3,
+            ImeddedDoubleQuoteIssue = 4,
+            EmptyRow = 5
+
+
+        }
+}
